5d640be2e0e77d7d2d68de287102094369960466
Javi Sastre <javi.sastre.cifo@gmail.com>
2019-07-22 09:48:37 +0200

    Initial commit

---
+++ Pac6/src/Bank/Accounts.java
@@ -0,0 +1,25 @@
+package Bank;
+
+public class Accounts {
+
+	public static void main(String[] args) {
+
+	}
+
+	// transfereix un valor de un compte a un altre amb un return de //l’operació
+	public static void transfer() {
+
+	}
+
+	// depèn del valor del compte decideix si es pot concedir un crèdit o //no a
+	// l’usuari
+	public static void personalCredit() {
+
+	}
+
+	// afegeix un mètode nou
+	public static void yourMethod() {
+
+	}
+
+}
